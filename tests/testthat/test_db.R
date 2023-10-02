534f5483fe8a61ec26614869b538213fe1a657d6
Matthijs Berends <m.s.berends@umcg.nl>
2023-10-02 12:26:40 +0200

    fix unit test

--- tests/testthat/test_db.R
+++ tests/testthat/test_db.R
@@ -50,14 +50,14 @@ test_that("db works", {
   db |> db_write_table("my_iris_table", values = iris, overwrite = TRUE)
   expect_true("my_iris_table" %in% db_list_tables(db))
   expect_true(db_has_table(db, "my_iris_table"))
-  
-  db |> db_drop_table("my_iris_table")
-  expect_false("my_iris_table" %in% db_list_tables(db))
-  db |> db_close()
 
   df <- get_duckdb_data(NULL, Species == "setosa", duckdb_path = "my_duck.db", duckdb_table = "my_iris_table")
   expect_output(print(df))
   expect_output(print(certedb_query(df)))
+  
+  db |> db_drop_table("my_iris_table")
+  expect_false("my_iris_table" %in% db_list_tables(db))
+  db |> db_close()
   
   # remove the database
   unlink("my_duck.db")
